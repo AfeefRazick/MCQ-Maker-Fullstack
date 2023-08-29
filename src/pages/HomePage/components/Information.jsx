4f52872a2626d3f4786b498f4d84ab760b228a78
AfeefRazick <afeefrazickamir@gmail.com>
2023-08-29 22:25:40 +0530

    home page style

--- src/pages/HomePage/components/Information.jsx
+++ src/pages/HomePage/components/Information.jsx
@@ -2,7 +2,7 @@ import designurl from "/design-page.png"
 
 export default function Information() {
   return (
-    <div className=" flex w-full flex-wrap items-center justify-center sm:px-16 md:flex-nowrap md:justify-between">
+    <div className=" flex h-screen w-full flex-wrap items-center justify-center sm:px-16 md:flex-nowrap md:justify-between">
       <p className="mt-10 w-4/5 text-lg md:px-16 lg:px-20 xl:px-24">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis
         voluptas quam consectetur ullam iure. Commodi saepe officia quis
